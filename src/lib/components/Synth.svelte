6b8647c060fdc6757f5e9ca10476d37711029f35
Félix Girault <felix.girault@gmail.com>
2023-04-10 19:32:46 +0200

    Fixed synth output
    Playing the same note twice did not produce any sound, due to the use of
    writable stores as event sources.
    When setting the same value twice, no event was emitted because the
    value did not change.
    We're now using a diff of notes so it changes whatever happens.

--- src/lib/components/Synth.svelte
+++ src/lib/components/Synth.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
+	import type {DiffData} from 'fast-array-diff';
 	import {onMount} from 'svelte';
 	import {PolySynth, Synth} from 'tone';
+	import type {Note} from '$lib/notes';
 	import {playedNotes} from '$lib/stores';
 
-	const {started, stopped} = playedNotes;
+	const {diff} = playedNotes;
 	let synth: PolySynth;
+
+	const triggerNotes = (diff: DiffData<Note>) => {
+		synth?.triggerAttack(diff.added);
+		synth?.triggerRelease(diff.removed);
+	};
 
 	onMount(() => {
 		synth = new PolySynth(Synth, {
@@ -14,6 +21,5 @@
 		}).toDestination();
 	});
 
-	$: $started && synth?.triggerAttack($started);
-	$: $stopped && synth?.triggerRelease($stopped);
+	$: triggerNotes($diff);
 </script>
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,26 +1,39 @@
-import {readonly, writable} from 'svelte/store';
+import {diff} from 'fast-array-diff';
+import {
+	type Readable,
+	derived,
+	get,
+	readonly,
+	writable
+} from 'svelte/store';
 import {withValue, withoutValue} from './arrays';
 import type {Note} from './notes';
 
+const diffStore = <T>(store: Readable<T[]>) => {
+	let previous = get(store);
+
+	return derived(store, ($values) => {
+		const d = diff(previous, $values);
+		previous = $values;
+		return d;
+	});
+};
+
 const playableNotesStore = () => {
 	const notes = writable<Note[]>([]);
-	const started = writable<Note | undefined>();
-	const stopped = writable<Note | undefined>();
+	const diff = diffStore(notes);
 
 	const start = (note: Note) => {
 		notes.update(($notes) => withValue($notes, note));
-		started.set(note);
 	};
 
 	const stop = (note: Note) => {
 		notes.update(($notes) => withoutValue($notes, note));
-		stopped.set(note);
 	};
 
 	return {
 		notes: readonly(notes),
-		started: readonly(started),
-		stopped: readonly(stopped),
+		diff,
 		start,
 		stop
 	};
