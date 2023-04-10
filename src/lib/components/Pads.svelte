d14ca0c5e4b58b4cff7f8dc6c5eaf2572afd33d6
Félix Girault <felix.girault@gmail.com>
2023-04-11 00:55:05 +0200

    Added touch support to play notes
    This is pretty raw for now but it gets the job done.
    A nice thing to add next would be to take pressure into account to
    control velocity.

--- src/lib/components/Pads.svelte
+++ src/lib/components/Pads.svelte
@@ -2,6 +2,8 @@
 	import type {Note} from '$lib/notes';
 	import {playedNotes} from '$lib/stores';
 
+	const touches = new Map<number, Note>();
+
 	// In case the user started selecting text, deselects it
 	// so it doesn't mess with the pads UI.
 	const deselectCurrentText = () => {
@@ -20,20 +22,82 @@
 		}
 
 		const element = target as HTMLElement;
-		const note = element?.dataset?.note;
+		const note = element?.dataset?.note as Note | undefined;
 
 		if (note) {
 			deselectCurrentText();
-			playedNotes.start(note as Note);
+			playedNotes.start(note);
 		}
 	};
 
 	const handleStop = ({target}: MouseEvent) => {
 		const element = target as HTMLElement;
-		const note = element?.dataset?.note;
+		const note = element?.dataset?.note as Note | undefined;
 
 		if (note) {
-			playedNotes.stop(note as Note);
+			playedNotes.stop(note);
+		}
+	};
+
+	const handleTouchStart = (event: TouchEvent) => {
+		event.preventDefault();
+
+		for (let i = 0; i < event.changedTouches.length; i++) {
+			const touch = event.changedTouches[i];
+			const element = touch.target as HTMLElement;
+			const note = element?.dataset?.note as Note | undefined;
+
+			if (note) {
+				playedNotes.start(note);
+				touches.set(touch.identifier, note);
+			}
+		}
+	};
+
+	// @TODO Debounce this to avoid too many calls to
+	// `document.elementFromPoint()`.
+	const handleTouchMove = (event: TouchEvent) => {
+		event.preventDefault();
+
+		for (let i = 0; i < event.changedTouches.length; i++) {
+			const touch = event.changedTouches[i];
+			const element = document.elementFromPoint(
+				touch.clientX,
+				touch.clientY
+			) as HTMLElement;
+			const note = element?.dataset?.note as Note | undefined;
+			const previousNote = touches.get(touch.identifier);
+
+			if (note !== previousNote) {
+				if (previousNote) {
+					playedNotes.stop(previousNote);
+					touches.delete(touch.identifier);
+				}
+
+				if (note) {
+					playedNotes.start(note);
+					touches.set(touch.identifier, note);
+				}
+			}
+		}
+	};
+
+	const handleTouchEnd = (event: TouchEvent) => {
+		event.preventDefault();
+
+		for (let i = 0; i < event.changedTouches.length; i++) {
+			const touch = event.changedTouches[i];
+			const element = document.elementFromPoint(
+				touch.clientX,
+				touch.clientY
+			) as HTMLElement;
+			const note = element?.dataset?.note as Note | undefined;
+
+			if (note) {
+				playedNotes.stop(note);
+			}
+
+			touches.delete(touch.identifier);
 		}
 	};
 </script>
@@ -43,6 +107,9 @@
 	on:mousedown={handleStart}
 	on:mouseup={handleStop}
 	on:mouseout={handleStop}
+	on:touchstart={handleTouchStart}
+	on:touchmove={handleTouchMove}
+	on:touchend={handleTouchEnd}
 	on:focus={() => {}}
 	on:blur={() => {}}
 >
