785877123554b2e6f82f50e432161944a28110ab
Félix Girault <felix.girault@gmail.com>
2023-04-11 18:26:46 +0200

    Added fretboard fingerings
    There is still many edge-cases to enhance, but it gets the job done for
    now.

--- src/lib/fretboard.ts
+++ src/lib/fretboard.ts
@@ -1,4 +1,16 @@
 import {sum} from './arrays';
+import {
+	type Note,
+	type PitchClass,
+	PitchClasses,
+	notePitchClass
+} from './notes';
+import type {Scale} from './scales';
+
+export type Fretboard = {
+	tuning: Note[];
+	fretCount: number;
+};
 
 // Magic constant representing the relationship in size
 // between two frets.
@@ -18,4 +30,95 @@ export const fretRatios = (count: number) => {
 
 	const total = sum(ratios);
 	return ratios.map((r) => (r / total) * 100);
+};
+
+// Finds a playable fingering for the given scale, ensuring
+// there is not too much notes per string, and there is no
+// excessive finger stretch between notes.
+export const fingering = (
+	fretboard: Fretboard,
+	scale: Scale,
+	root: PitchClass,
+	{
+		maxNotesPerString = 4,
+		maxStretch = 4
+	}: {
+		maxNotesPerString?: number;
+		maxStretch?: number;
+	} = {}
+) => {
+	// In the rest of the function, PCI stands for "pitch
+	// class index", to shorten variable names a bit.
+	// Everything is computed by comparing the index of pitch
+	// classes in the full pitch class list, as to speed up
+	// computations by avoiding comparing notes, transposing
+	// them, etc.
+	const rootPci = PitchClasses.indexOf(root);
+	const openPcis = fretboard.tuning.map((note) =>
+		PitchClasses.indexOf(notePitchClass(note))
+	);
+
+	const scalePcis = scale.chroma
+		.split('')
+		.reduce(
+			(pcis, chroma, i) =>
+				chroma === '1'
+					? pcis.concat((rootPci + i) % PitchClasses.length)
+					: pcis,
+			[] as number[]
+		);
+
+	const indices: number[][] = openPcis.map(() => []);
+
+	let nextScalePciIndex = 0;
+	let rootFretIndex = -1;
+
+	for (
+		let stringIndex = 0;
+		stringIndex < openPcis.length;
+		stringIndex++
+	) {
+		const openPci = openPcis[stringIndex];
+		let pressedFretCount = 0;
+
+		for (
+			let fretIndex = 0;
+			fretIndex < fretboard.fretCount;
+			fretIndex++
+		) {
+			const currentPci =
+				(openPci + fretIndex) % PitchClasses.length;
+			const nextScalePci = scalePcis[nextScalePciIndex];
+
+			// Skips notes until the root is found.
+			if (rootFretIndex < 0) {
+				if (currentPci === nextScalePci) {
+					rootFretIndex = fretIndex;
+				} else {
+					continue;
+				}
+			}
+
+			if (currentPci !== nextScalePci) {
+				continue;
+			}
+
+			// Is the finger stretch required to play this note
+			// too wide?
+			if (Math.abs(rootFretIndex - fretIndex) > maxStretch) {
+				break;
+			}
+
+			pressedFretCount++;
+			nextScalePciIndex =
+				(nextScalePciIndex + 1) % scalePcis.length;
+			indices[stringIndex].push(fretIndex);
+
+			if (pressedFretCount >= maxNotesPerString) {
+				break;
+			}
+		}
+	}
+
+	return indices;
 };
