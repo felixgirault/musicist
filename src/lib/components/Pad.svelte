2498e98719468ec6333612a8c36cc64175d8f5e4
Félix Girault <felix.girault@gmail.com>
2023-04-15 13:43:01 +0200

    Improved pad styles
    Scale notes and roots are now easier to identify.

--- src/lib/components/Pad.svelte
+++ src/lib/components/Pad.svelte
@@ -27,7 +27,10 @@
 
 <style>
 	button {
-		--scale-note-background: var(--outline);
+		--note-name-background: transparent;
+		--note-name-color: var(--button-color);
+		--scale-note-background: transparent;
+		--scale-note-indicator-background: var(--outline);
 		--scale-note-size: var(--spacing-0_5);
 		--scale-note-direction: to bottom;
 		display: inline-flex;
@@ -36,10 +39,16 @@
 		box-shadow: inset 0 0 0 1px var(--surface),
 			0 0 0 1px var(--surface);
 		background-image: linear-gradient(
-			var(--scale-note-direction),
-			var(--scale-note-background) 0 var(--scale-note-size),
-			transparent var(--scale-note-size) 100%
-		);
+				var(--scale-note-direction),
+				var(--scale-note-indicator-background) 0
+					var(--scale-note-size),
+				transparent var(--scale-note-size) 100%
+			),
+			linear-gradient(
+				0deg,
+				var(--scale-note-background) 0 100%
+			),
+			linear-gradient(0deg, var(--button-background) 0 100%);
 	}
 
 	button.flipped {
@@ -49,16 +58,23 @@
 
 	button span {
 		pointer-events: none;
+		outline: var(--spacing-0_25) solid
+			var(--note-name-background);
+		border-radius: var(--radius);
+		background: var(--note-name-background);
+		color: var(--note-name-color);
+		line-height: 1;
 		font-size: var(--font-size-small);
 	}
 
-	.root {
+	.root span {
+		--note-name-background: var(--primary);
+		--note-name-color: var(--button-background);
 		font-weight: bolder;
 	}
 
 	.scale {
-		--scale-note-background: var(
-			--interactive-surface--active
-		);
+		--scale-note-background: rgba(var(--primary-rgb), 0.2);
+		--scale-note-indicator-background: var(--primary);
 	}
 </style>
