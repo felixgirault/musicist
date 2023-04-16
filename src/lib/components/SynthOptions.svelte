620f170f696b91abfb5105fbd9c771895f2b67d5
Félix Girault <felix.girault@gmail.com>
2023-04-16 20:14:11 +0200

    Showing the cheat option only when a scale is selected

--- src/lib/components/SynthOptions.svelte
+++ src/lib/components/SynthOptions.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-	import {areOutOfScaleNotesMuted} from '$lib/stores';
+	import {areOutOfScaleNotesMuted, scale} from '$lib/stores';
 </script>
 
-<div>
-	<label for="SynthOptions-mute">Cheat</label>
-	<input
-		id="SynthOptions-mute"
-		type="checkbox"
-		name="mute"
-		bind:checked={$areOutOfScaleNotesMuted}
-	/>
-</div>
+{#if $scale}
+	<div>
+		<label for="SynthOptions-mute">Cheat</label>
+		<input
+			id="SynthOptions-mute"
+			type="checkbox"
+			name="mute"
+			bind:checked={$areOutOfScaleNotesMuted}
+		/>
+	</div>
+{/if}
